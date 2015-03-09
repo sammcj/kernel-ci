aa0d35e5568b087f5ccd55476a4b9cec0e7ce35f
Sam McLeod <smj@fastmail.com>
2015-03-09 12:15:25 +1100

    Refactoring and parameterisation
    Relates to #2

--- buildkernel.sh
+++ buildkernel.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
-#
+set -xe
+
 # Much of this script was sourced from
 # forums.debian.net/viewtopic.php?f=16&t=87006
 # then *heavily* modified to support CI and Docker
@@ -17,7 +18,83 @@
 # easily comment them out to skip them or to run manual steps (for
 # example changing the kernel configuration with "make menuconfig").
 
-set -xe
+# Please submit bugs and pull requests to
+# https://github.com/sammcj/kernel-ci
+
+# Optional variables that may be passed to this script:
+
+# KERNEL_VERSION
+# DEFAULT VALUE: Latest STABLE kernel version
+#
+# VERSION_POSTFIX
+# For restrictions see the --append-to-version option of make-kpg.c
+# DEFAULT VALUE: "-ci"
+
+# TRUSTED_FINGERPRINT
+# Fingerprint of a trusted key the kernel is signed with
+# See http://www.kernel.org/signature.html
+#     http://lwn.net/Articles/461647/
+# ATTENTION: Make sure you really trust it!
+# DEFAULT VALUE: "ABAF 11C6 5A29 70B1 30AB  E3C4 79BE 3E43 0041 1886"
+
+# PACKAGE_NAME
+# DEFAULT VALUE: "linux-image-$KERNEL_VERSION-$VERSION_POSTFIX.deb"
+
+# SOURCE_URL_BASE
+# Where the archive and sources are located
+# DEFAULT VALUE: "https://kernel.org/pub/linux/kernel/v3.x"
+
+# KEYSERVER
+# Server used to get the trusted key from.
+# DEFAULT VALUE: "hkp://pool.sks-keyservers.net"
+
+# BUILD_ONLY_LOADED_MODULES
+# Set to yes if you want to build only the modules that are currently
+# loaded Speeds up the build. But modules that are not currently
+# loaded will be missing!  Only usefull if you really have to speed up
+# the build time and the kernel is intended for the running system and
+# the hardware is not expected to change.
+# DEFAULT VALUE: "no"
+
+### POST PROCESSING ###
+
+# PACKAGECLOUD
+# Enable pushing to reprepro upon successful build
+# DEFAULT VALUE: "false"
+
+# PACKAGE_CLOUD_URL
+# Must be replaced if you wish to upload to packagecloud.io
+# DEFAULT VALUE: "mrmondo/debian-kernel/debian/jessie"
+
+# REPREPRO
+# Enable pushing to reprepro upon successful build
+# DEFAULT VALUE: "false"
+
+# REREPRO_HOST
+# The username and password to login to the reprepro host
+# DEFAULT VALUE: "ci@aptproxy"
+
+# REPREPO_URL
+# The URL of the reprepro mirror
+# DEFAULT VALUE: "var/vhost/mycoolaptmirror.com/html"
+
+# -------------VARIABLES---------------
+
+TRUSTED_FINGERPRINT='ABAF 11C6 5A29 70B1 30AB  E3C4 79BE 3E43 0041 1886'
+VERSION_POSTFIX=${VERSION_POSTFIX-"-ci"}
+PACKAGE_NAME="linux-image-$KERNEL_VERSION-$VERSION_POSTFIX.deb"
+SOURCE_URL_BASE=${SOURCE_URL_BASE-"https://kernel.org/pub/linux/kernel/v3.x"}
+KEYSERVER=hkp://pool.sks-keyservers.net
+BUILD_ONLY_LOADED_MODULES=${BUILD_ONLY_LOADED_MODULES-"no"}
+PACKAGECLOUD=${PACKAGECLOUD-"true"}
+REPREPRO=${REPREPRO-"false"}
+PACKAGE_CLOUD_URL=${PACKAGE_CLOUD_URL-"mrmondo/debian-kernel/debian/jessie"}
+REREPRO_HOST=${REREPRO_HOST-"ci@aptproxy"}
+REPREPO_URL=${REPREPO_URL-"var/vhost/mycoolaptmirror.com/html"}
+
+if [ -z "$KERNEL_VERSION" ]; then
+  KERNEL_VERSION=$(curl --silent https://www.kernel.org/finger_banner | awk '{print $11}'| head -2|tail -1)
+fi
 
 # -------------PRE-FLIGHT---------------
 
@@ -41,8 +118,7 @@ DownloadManager() {
 }
 
 # Are we running in Docker?
-# If not, set the default build dir (where the git repo is checked out)
-# to $HOME
+# If not, set the default build dir (where the git repo is checked out) to $HOME
 BuildEnv() {
   if [ -f /.dockerinit ]; then
     echo "Detected Docker"
@@ -51,43 +127,6 @@ BuildEnv() {
     echo "Not running in Docker"
     export BUILD_DIR=$(pwd)
   fi
-}
-
-
-# --------------------SETUP--------------------
-
-#
-# Fingerprint of a trusted key the kernel is signed with.
-# See http://www.kernel.org/signature.html
-#     http://lwn.net/Articles/461647/
-# ATTENTION: Make sure you really trust it!
-TRUSTED_FINGERPRINT='ABAF 11C6 5A29 70B1 30AB  E3C4 79BE 3E43 0041 1886'
-
-# Builds the latest upstream kernel version if not set.
-if [ -z "$KERNEL_VERSION" ]; then
-  KERNEL_VERSION=$(curl --silent https://www.kernel.org/finger_banner | awk '{print $11}'| head -2|tail -1)
-fi
-
-# Default string that is appended to the version if not set. For restrictions see the --append-to-version option of make-kpg.
-VERSION_POSTFIX=${VERSION_POSTFIX-"-ci"}
-PACKAGE_NAME="linux-image-$KERNEL_VERSION-$VERSION_POSTFIX.deb"
-
-# Default URL where the archive and sources are located if not set.
-SOURCE_URL_BASE=${SOURCE_URL_BASE-"https://kernel.org/pub/linux/kernel/v3.x"}
-
-# Server used to get the trusted key from.
-KEYSERVER=hkp://pool.sks-keyservers.net
-
-# Set to yes if you want to build only the modules that are currently
-# loaded Speeds up the build. But modules that are not currently
-# loaded will be missing!  Only usefull if you really have to speed up
-# the build time and the kernel is intended for the running system and
-# the hardware is not expected to change.
-BUILD_ONLY_LOADED_MODULES=no
-
-function InstallPreqs()
-{
-  echo "You need the following packages installed fakeroot make build-essential kernel-package for this script to work"
 }
 
 # --------------DOWNLOAD------------------
@@ -121,7 +160,7 @@ function VerifyExtract()
   echo "Extracting downloaded sources to tar..."
   [ -f linux-$KERNEL_VERSION.tar ] || unxz --keep linux-$KERNEL_VERSION.tar.xz
 
-  # Commented out as often fails for no reason - would be nice to have though
+  #Commented out as often fails for no reason - would be nice to have see issue #1
   #echo "Verifying tar is signed with the trusted key..."
   #gpg -v --trusted-key 0x${TRUSTEDLONGID:24} --verify linux-$KERNEL_VERSION.tar.sign
 
@@ -163,9 +202,6 @@ function Build()
 {
   pushd ./linux-$KERNEL_VERSION
 
-  # See the following links for more information:
-  # http://www.debian.org/doc/manuals/debian-faq/ch-kernel.en.html
-  # http://www.debian.org/releases/stable/amd64/ch08s06.html.en
   echo "Now building the kernel, this will take a while..."
   time fakeroot make-kpkg --jobs `getconf _NPROCESSORS_ONLN` --append-to-version "$VERSION_POSTFIX" --initrd kernel_image
   mv ../linux-image*.deb ../$PACKAGE_NAME
@@ -187,16 +223,16 @@ function Sum()
 # --------------EXPORT------------------
 
 # Copies the package to the apt repo and imports it remotely with reprepro
-function Push()
+function RepreproPush()
 {
   pwd
   pushd ./linux-$KERNEL_VERSION
 
-  scp $PACKAGE_NAME ci@aptproxy:/var/tmp
-  ssh ci@aptproxy reprepro -A all -Vb /var/vhost/mycoolaptmirror.com/html /var/tmp/$PACKAGE_NAME
+  scp $PACKAGE_NAME $REREPRO_HOST:/var/tmp
+  ssh $REREPRO_HOST reprepro -A all -Vb $REPREPO_URL /var/tmp/$PACKAGE_NAME
   popd
 
-  echo "Image pushed to int-proxy and imported to reprepro"
+  echo "Image pushed to $REREPRO_HOST and imported to reprepro"
 }
 
 # Pushes the package to packagecloud.io
@@ -205,8 +241,8 @@ function PackageCloud()
   pwd
   pushd ./linux-$KERNEL_VERSION
   # Remove the package if it already exists with the same name
-  package_cloud yank mrmondo/debian-kernel/debian/jessie $PACKAGE_NAME || true
-  package_cloud push mrmondo/debian-kernel/debian/jessie $PACKAGE_NAME && echo "Package uploaded to https://packagecloud.io/mrmondo/debian-kernel?filter=debs"
+  package_cloud yank $PACKAGE_CLOUD_URL $PACKAGE_NAME || true
+  package_cloud push $PACKAGE_CLOUD_URL $PACKAGE_NAME
   popd
 }
 
@@ -215,12 +251,15 @@ function PackageCloud()
 # Run all function
 CheckFreeSpace
 BuildEnv
-InstallPreqs
 RecvKey
 DownloadSources
 VerifyExtract
 SetCurrentConfig
 Build
+
+if [ "$REPREPRO" = "true" ]; then
+  RepreproPush;
+fi
 
 if [ "$PACKAGECLOUD" = "true" ]; then
   PackageCloud;
